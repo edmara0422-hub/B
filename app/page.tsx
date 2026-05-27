69a114cff43f07005ef81e3ec4a55784e002e501
Edmara Rocha <edmara0422@gmail.com>
2026-05-27 10:04:11 -0300

    s

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/navigation';
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
-    router.push('/design-space');
+    router.push('/home');
   }, [router]);
   return <div style={{ backgroundColor: '#000000', width: '100vw', height: '100vh' }}></div>;
 }
